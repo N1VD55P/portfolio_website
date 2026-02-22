e8d93102416b7c1335f5be1fc5b355afeec30af3
N1VD55P <n1vdeepnain@gmail.com>
2026-02-22 14:19:20 +0530

    under work pls wait

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const countdownDate = new Date();
-countdownDate.setDate(countdownDate.getDate() + 8);
+const countdownDate = new Date('March 2, 2026 00:00:00').getTime();
 
 const daysElement = document.getElementById('days');
 const hoursElement = document.getElementById('hours');
@@ -8,7 +7,7 @@ const secondsElement = document.getElementById('seconds');
 
 function updateCountdown() {
     const now = new Date().getTime();
-    const distance = countdownDate.getTime() - now;
+    const distance = countdownDate - now;
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -34,4 +33,4 @@ updateCountdown();
 const countdownInterval = setInterval(updateCountdown, 1000);
 
 console.log('Countdown started!');
-console.log('Launch date:', countdownDate.toLocaleString());
+console.log('Launch date:', new Date(countdownDate).toLocaleString());
